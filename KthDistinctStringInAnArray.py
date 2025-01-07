6d3b8954eee618e9a7b7d17fd93c5366e837436e
Alan-Muk <alanmukab@gmail.com>
2025-01-07 07:39:19 +0300

    NeetCodeIO

--- KthDistinctStringInAnArray.py
+++ KthDistinctStringInAnArray.py
@@ -1,19 +1,32 @@
 class Solution:
 	def kthDistinct(self, arr:list[str], k:int) -> str:
 
-		for i in range(len(arr)):
-			distinct_flag = True
-			for j in range(len(arr)):
-				if i == j:
-					continue
+		# for i in range(len(arr)):
+		# 	distinct_flag = True
+		# 	for j in range(len(arr)):
+		# 		if i == j:
+		# 			continue
 				
-				if arr[i] == arr[j]:
-					distinct_flag = False
-					break
+		# 		if arr[i] == arr[j]:
+		# 			distinct_flag = False
+		# 			break
 
-			if distinct_flag:
+		# 	if distinct_flag:
+		# 		k -= 1
+		# 		if k == 0:
+		# 			return arr[i]
+
+		# return ""
+		count = defaultdict(int)
+
+		for s in arr:
+			count[s] += 1
+
+		for s in arr:
+			if count[s] == 1:
 				k -= 1
-				if k == 0:
-					return arr[i]
+
+			if k == 0:
+				return s
 
 		return ""

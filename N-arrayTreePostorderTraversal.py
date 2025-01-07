f1f1a9f7df9f02edbfc511c6c0278994bd45cc30
Alan-Muk <alanmukab@gmail.com>
2025-01-07 08:05:32 +0300

    NeetCodeIO

--- N-arrayTreePostorderTraversal.py
+++ N-arrayTreePostorderTraversal.py
@@ -3,10 +3,38 @@ class Node:
 		self.val = val
 		self.children = children
 
+# class Solution:
+# 	def postorder(self, root:Node) -> list[int]:
+# 		res = []
+
+# 		def helper(node):
+# 			if not node:
+# 				return
+
+# 			for c in node.children:
+# 			 	helper(c)
+# 			res.append(node.val)
+# 		helper(root)
+# 		return res
 class Solution:
 	def postorder(self, root:Node) -> list[int]:
 		res = []
-		
+
+		if not root:
+			return []
+
+		stack = [(root, False)]
+
+		while stack:
+			node, visited = stack.pop()
+			if visited:
+				res.append(node.val)
+			else:
+				stack.append((node, True))
+				for c in node.children[::-1]:
+					stack.append((c, False))
+		return res
+
 		def helper(node):
 			if not node:
 				return

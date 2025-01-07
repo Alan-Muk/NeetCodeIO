efe4ab1908e77f2abb62b0c59c7311112145cd9f
Alan-Muk <alanmukab@gmail.com>
2025-01-07 07:57:42 +0300

    NeetCodeIO

---
+++ N-arrayTreePostorderTraversal.py
@@ -0,0 +1,18 @@
+class Node:
+	def __init__(self, val=None, children=None):
+		self.val = val
+		self.children = children
+
+class Solution:
+	def postorder(self, root:Node) -> list[int]:
+		res = []
+		
+		def helper(node):
+			if not node:
+				return
+
+			for c in node.children:
+			 	helper(c)
+			res.append(node.val)
+		helper(root)
+		return res

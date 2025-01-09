629bf27f29135e457fe4213109f57d287da5c110
Alan-Muk <alanmukab@gmail.com>
2025-01-09 08:37:58 +0300

    NeetCodeIO

---
+++ DefuseTheBomb.py
@@ -0,0 +1,15 @@
+class Solution:
+	def decrypt(self, code:list[int], k:int) -> list[int]:
+		
+		N = len(code)
+		res = [0] * N
+
+		for i in range(N):
+			if k > 0:
+				for j in range(i+1, i+1+k):
+					res[i] += code[j % N]
+			elif k < 0:
+				for j in range(i - 1, i - 1 -abs(k), -1):
+					res[i] += code[j % N]
+
+		return res

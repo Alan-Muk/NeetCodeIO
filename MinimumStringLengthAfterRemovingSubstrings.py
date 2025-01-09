6d0e28e3e17451beb4e81c37523b6a503bd332fd
Alan-Muk <alanmukab@gmail.com>
2025-01-09 08:23:03 +0300

    NeetCodeIO

---
+++ MinimumStringLengthAfterRemovingSubstrings.py
@@ -0,0 +1,13 @@
+class Solution:
+	def minimumString(self, s:str) -> int:
+		stack = []
+
+		for c in s:
+			stack.append(c)
+			
+			if len(stack) >= 2 and ((stack[-2] == "A" and stack[-1] == "B") or 
+				(stack[-2] == "C" and stack[-1] == "D")):
+				stack.pop()
+				stack.pop()
+
+		return len(stack)

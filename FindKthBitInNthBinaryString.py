5a4f4af57171551c6454670b6bc758faeba30ffa
Alan-Muk <alanmukab@gmail.com>
2025-01-10 08:52:18 +0300

    NeetCodeIO

---
+++ FindKthBitInNthBinaryString.py
@@ -0,0 +1,19 @@
+class Solution:
+	def KthBitInNthBinary(self, n:int, k:int) -> str:
+		length = 2**n - 1
+
+		def helper(length, k):
+			if length == 1:
+				return "0"
+
+			half = length // 2
+
+			if k <= half:
+				return helper(half, k)
+			elif k >= half + 1:
+				res = helper(half, 1 + length - k)
+				return "0" if res == "1" else "1"
+			else:
+				return "1"
+
+		return helper(length, k)

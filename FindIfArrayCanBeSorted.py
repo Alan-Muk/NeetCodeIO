22131c7e26308957a66907b4a723f3cd05f52ffa
Alan-Muk <alanmukab@gmail.com>
2025-01-14 09:07:17 +0300

    NeetCodeIO

---
+++ FindIfArrayCanBeSorted.py
@@ -0,0 +1,22 @@
+class Solution:
+	def canSortArray(self, nums:list[int]) -> bool:
+
+		def count_bits(n):
+			return bin(n).count("1")
+
+		cur_min, cur_max = nums[0], nums[0]
+		prex_max = float("-inf")
+
+		for n in nums:
+			if count_bits(n) == count_bits(cur_min):
+				cur_min = min(cur_min, n)
+				cur_max = max(cur_max, n)
+			else:
+				if cur_min < prex_max:
+					return False
+				prex_max = cur_max
+				cur_min, cur_max = n, n
+			if cur_min < prex_max:
+				return False
+
+		return True

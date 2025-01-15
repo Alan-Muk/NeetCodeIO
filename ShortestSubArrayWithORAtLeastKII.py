e5fea6be81d6c531ede3009a862ec48c22a36c46
Alan-Muk <alanmukab@gmail.com>
2025-01-15 09:02:22 +0300

    NeetCodeIO

---
+++ ShortestSubArrayWithORAtLeastKII.py
@@ -0,0 +1,29 @@
+class Solution:
+	def minimumSubarrayLength(self, nums:list[int], k:int) -> int:
+		res = float("inf")
+		bits = [0] * 32
+
+		def bits_update(bits, n, diff):
+			for i in range(32):
+				if n & (1 << i):
+					bits[i] += diff
+
+		def bit_to_int(bits):
+			res = 0
+			for i in range(32):
+				if bits[i]:
+					res += (1 << i)
+			return res
+
+		l = 0
+		for r in range(len(nums)):
+			bits_update(bits, nums[r], 1)
+			cur_or = bit_to_int(bits)
+
+			while l <= r and cur_or >= k:
+				res = min(res, r - l + 1)
+				bits_update(bits, nums[l], -1)
+				cur_or = bit_to_int(bits)
+				l += 1
+
+		return -1 if res == float("inf") else res

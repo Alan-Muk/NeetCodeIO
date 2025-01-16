f1366fa6b6883420d86e188cd0d92410b6fd521b
Alan-Muk <alanmukab@gmail.com>
2025-01-16 08:29:27 +0300

    NeetCodeIO

--- MostBeautifulItemForEachQuery.py
+++ MostBeautifulItemForEachQuery.py
@@ -1,15 +1,34 @@
 class Solution:
 	def maximumBeauty(self, items:list[list[int]], queries:list[int]) -> list[int]:
-		res = []
+		items.sort() #[p,b]
+		queries = [(q, i) for i, q in enumerate(queries)]
+		queries.sort()
 
-		for q in queries:
-			max_beaut = 0
+		res = [0] * len(queries)
+		max_beaut = 0
+		j = 0
+		for q, i in queries:
+			while j < len(items) and items[j][0] <= q:
+				max_beaut = max(max_beaut, items[j][1])
+				j += 1
 
-			for p, b in items:
-				if p <= q:
-					max_beaut = max(max_beaut, b)
 
-			res.append(max_beaut)
+			res[i] = max_beaut
+
+		return res	
 
 
-		return res
+
+		# res = []
+
+		# for q in queries:
+		# 	max_beaut = 0
+
+		# 	for p, b in items:
+		# 		if p <= q:
+		# 			max_beaut = max(max_beaut, b)
+
+		# 	res.append(max_beaut)
+
+
+		# return res

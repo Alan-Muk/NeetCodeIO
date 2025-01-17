e6f758131d6eeef9c883db4b9c7f3bcd4190bc28
Alan-Muk <alanmukab@gmail.com>
2025-01-17 08:41:17 +0300

    NeetCodeIO

---
+++ MaximumSumOfDistinctSubArraysWithLengthK.py
@@ -0,0 +1,23 @@
+class Solution:
+	def maximimSubarraySum(self, nums:list[int], k:int) -> int:
+		res = 0
+
+		count = defaultdict(int)
+		cur_sum = 0
+		l = 0
+
+		for r in range(len(nums)):
+			cur_sum += nums[r]
+			count[nums[r]] += 1
+
+			if r - l + 1 > k:
+				count[nums[l]] -= 1
+				if count[nums[l]] == 0:
+					count.pop(nums[l])
+				cur_sum -= nums[l]
+				l += 1
+
+			if len(count) == r - l + 1 == k:
+				res = max(res, cur_sum)
+
+		return res

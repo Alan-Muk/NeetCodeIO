02763528b1dfa51550863583a890d3c3183f2ddd
Alan-Muk <alanmukab@gmail.com>
2025-01-21 07:59:15 +0300

    NeetCodeIO

---
+++ ReverseOddLevelBinaryTree.py
@@ -0,0 +1,30 @@
+class TreeNode:
+	def __init__(self, val=0, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
+
+class Solution:
+	def reverseOddLevel(self, root:TreeNode) -> TreeNode:
+		q = deque([root])
+
+		i = 0
+		while q:
+			if i & 1:
+				l, r = 0, len(q) - 1
+				while l <= r:
+					q[l].val, q[r].val = q[r].val, q[l].val
+					l += 1
+					r -= 1
+
+			for _ in range(len(q)):
+				node = q.popleft()
+				if node.left:
+					q.append(node.left)
+				if node.right:
+					q,append(node.right)
+
+			i += 1
+
+
+		return root

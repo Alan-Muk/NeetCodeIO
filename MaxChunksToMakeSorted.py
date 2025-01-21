bcb92d9454f0c457c6f125acd62adacd6433dfd4
Alan-Muk <alanmukab@gmail.com>
2025-01-21 07:53:17 +0300

    NeetCodeIO

---
+++ MaxChunksToMakeSorted.py
@@ -0,0 +1,12 @@
+class Solution:
+	def maxChunks(self, arr:list[int]) -> int:
+		cur_max = -1
+		res = 0
+
+		for i, n in enumerate(arr):
+			cur_max = max(n, cur_max)
+
+			if cur_max == i:
+				res += 1
+
+		return res

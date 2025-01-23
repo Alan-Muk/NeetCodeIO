d48713706d7b5210f7ee63d515a8c97d455683ea
Alan-Muk <alanmukab@gmail.com>
2025-01-23 07:59:32 +0300

    NeetCodeIO

---
+++ ShiftingLettersII.py
@@ -0,0 +1,19 @@
+class Solution:
+	def shiftLetters(self, s:str, shifts:list[list[int]]) -> str:
+		prefix_diff = [0] * (len(s) + 1)
+
+		for left, right, d in shifts:
+			prefix_diff[right + 1] += 1 if d else -1
+			prefix_diff[left] += -1 if diff else 1
+
+		diff = 0
+		res = [ord(c) - ord("a") for c in s]
+
+		for i in reversed(range(len(prefix_diff))):
+			diff += prefix_diff[i]
+
+			res[i - 1] = (diff + res[i - 1]) % 26
+
+
+		s = [chr(ord("a") + n) for n in res]
+		return "".join(s)
